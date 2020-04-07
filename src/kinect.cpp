94280074faaa98fb359bfb0536ed6ab056f4e00c
nozomi hiragi <32630547+nozomi-hiragi@users.noreply.github.com>
2020-04-07 12:15:01 +0900

    add BodySource

--- src/kinect.cpp
+++ src/kinect.cpp
@@ -1,6 +1,7 @@
 #include "ColorSource.h"
 #include "DepthSource.h"
 #include "BodyIndexSource.h"
+#include "BodySource.h"
 
 extern "C" void GDN_EXPORT kinect_gdnative_init(godot_gdnative_init_options *o) {
     godot::Godot::gdnative_init(o);
@@ -16,4 +17,5 @@ extern "C" void GDN_EXPORT kinect_nativescript_init(void *handle) {
     godot::register_class<godot::ColorSource>();
     godot::register_class<godot::DepthSource>();
     godot::register_class<godot::BodyIndexSource>();
+    godot::register_class<godot::BodySource>();
 }
